e41df79b8b6a5b6431f6ade9ff5b10c40a0942f5
Trameur <thibaud.mourey@gmail.com>
2026-03-22 18:57:26 +0100

    better min stat page

--- fashionsite/chardata/min_stats_view.py
+++ fashionsite/chardata/min_stats_view.py
@@ -17,11 +17,53 @@
 import json
 
 from chardata.min_stats import get_min_stats, set_min_stats, convert_dict_index_name_to_key
+from chardata.stat_icons import get_stat_icon_path
 from chardata.util import set_response, safe_int, get_char_or_raise, HttpResponseJson
 from fashionistapulp.dofus_constants import AGI_TARGETS, CRIT_TARGETS,\
     STAT_ORDER
 from fashionistapulp.structure import get_structure
 from django.utils.translation import gettext as _
+from static_s3.templatetags.static_s3 import static
+
+
+def _get_stat_icon_url(stat_key):
+    icon_path = get_stat_icon_path(stat_key)
+    return static(icon_path) if icon_path else ''
+
+
+def _get_adv_stat_icon_urls(structure, adv_stat):
+    stat_name_to_key = {stat.name: stat.key for stat in structure.get_stats_list()}
+    local_name = adv_stat.get('local_name', '')
+    ordered_stat_names = list(adv_stat.get('stats', []))
+
+    if local_name and ' + ' in local_name:
+        localized_name_to_stat_name = {
+            _(stat.name): stat.name for stat in structure.get_stats_list()
+        }
+        ordered_from_label = []
+        for localized_part in local_name.split(' + '):
+            stat_name = localized_name_to_stat_name.get(localized_part)
+            if stat_name in ordered_stat_names and stat_name not in ordered_from_label:
+                ordered_from_label.append(stat_name)
+        if len(ordered_from_label) == len(ordered_stat_names):
+            ordered_stat_names = ordered_from_label
+
+    if adv_stat.get('key') in ('sum_perc_res', 'sum_res'):
+        ordered_stat_names = [
+            stat_name for stat_name in ordered_stat_names
+            if stat_name not in ('% Neutral Resist', 'Neutral Resist')
+        ] + [
+            stat_name for stat_name in ordered_stat_names
+            if stat_name in ('% Neutral Resist', 'Neutral Resist')
+        ]
+
+    icon_urls = []
+    for stat_name in ordered_stat_names:
+        stat_key = stat_name_to_key.get(stat_name)
+        icon_url = _get_stat_icon_url(stat_key)
+        if icon_url:
+            icon_urls.append(icon_url)
+    return icon_urls
 
 def min_stats(request, char_id):
     char = get_char_or_raise(request, char_id)
@@ -31,9 +73,12 @@ def min_stats(request, char_id):
     
     stats = []
     for stat in structure.get_stats_list():
+        if stat.key.startswith('pvp'):
+            continue
         stat_to_add = {}
         stat_to_add['key'] = stat.key
         stat_to_add['name'] = _(stat.name)
+        stat_to_add['icon_url'] = _get_stat_icon_url(stat.key)
         stats.append(stat_to_add)
     
     stats = [stat for stat in
@@ -43,17 +88,28 @@ def min_stats(request, char_id):
     ap = {}
     ap['key'] = 'ap'
     ap['name'] = _('AP')
+    ap['icon_url'] = _get_stat_icon_url('ap')
     fixed_fields.append(ap)
     mp = {}
     mp['key'] = 'mp'
     mp['name'] = _('MP')
+    mp['icon_url'] = _get_stat_icon_url('mp')
     fixed_fields.append(mp)
     rangestat = {}
     rangestat['key'] = 'range'
     rangestat['name'] = _('Range')
+    rangestat['icon_url'] = _get_stat_icon_url('range')
     fixed_fields.append(rangestat)
+    hp = {}
+    hp['key'] = 'hp'
+    hp['name'] = _('HP')
+    hp['icon_url'] = _get_stat_icon_url('hp')
+    fixed_fields.append(hp)
     
     adv_min_fields = structure.get_adv_mins()
+    for stat in adv_min_fields:
+        stat['icon_urls'] = _get_adv_stat_icon_urls(structure, stat)
+        stat['icon_url'] = stat['icon_urls'][0] if stat['icon_urls'] else _get_stat_icon_url(stat.get('key'))
     
     return set_response(request,
                         'chardata/min_stats.html',
