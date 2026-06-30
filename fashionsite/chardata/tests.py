e32a6281d64bea6f1005900e375e25fbbae9cbe9
ThibaudM <82444971+Trameur@users.noreply.github.com>
2026-07-01 00:14:19 +0200

    guides: add "reading an item" guide (5 langs)

--- fashionsite/chardata/tests.py
+++ fashionsite/chardata/tests.py
@@ -138,7 +138,8 @@ class PublicRouteSmokeTests(TestCase):
                      '/quickstart/', '/smartbuild/', '/forgemagie/',
                      '/guides/', '/guides/getting-started/',
                      '/guides/how-it-works/', '/guides/stats-explained/',
-                     '/guides/game-modes/', '/guides/versions-explained/',
+                     '/guides/game-modes/', '/guides/reading-an-item/',
+                     '/guides/versions-explained/',
                      '/offline/', '/robots.txt', '/manifest.webmanifest',
                      '/sw.js', '/ads.txt']:
             with self.subTest(path=path):
