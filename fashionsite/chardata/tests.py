d4de3a938b3398f3057e1c51a7ccf855d73e5fe6
ThibaudM <82444971+Trameur@users.noreply.github.com>
2026-07-01 00:48:14 +0200

    guides: add comparing-builds guide (5 langs)

--- fashionsite/chardata/tests.py
+++ fashionsite/chardata/tests.py
@@ -140,7 +140,7 @@ class PublicRouteSmokeTests(TestCase):
                      '/guides/how-it-works/', '/guides/stats-explained/',
                      '/guides/game-modes/', '/guides/reading-an-item/',
                      '/guides/mono-vs-multi-element/', '/guides/gearing-up/',
-                     '/guides/versions-explained/',
+                     '/guides/comparing-builds/', '/guides/versions-explained/',
                      '/offline/', '/robots.txt', '/manifest.webmanifest',
                      '/sw.js', '/ads.txt']:
             with self.subTest(path=path):
